0dbf331d795606ee6f7618d54fcb247289ff3bf1
Anurag Shukla <anurag090697@gmail.com>
2024-09-15 23:12:14 +0530

    adding test-5

--- weekly_test-5/backend/index.js
+++ weekly_test-5/backend/index.js
@@ -3,17 +3,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
-
+import cors from "cors";
 const app = express();
 const port = 6565;
 
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.get("/getData", async (req, res) => {
+  res.send("sab changa si");
+});
+
 try {
-    const connection = mongoose.connect(process.env.MONGO_ID);
-    if (connection) {
-        app.listen(port, () => {
-            console.log(`Server running at port ${port} and Database connected`)
-        })
-    }
+  const connection = mongoose.connect(process.env.MONGO_ID);
+  if (connection) {
+    app.listen(port, () => {
+      console.log(`Server running at port ${port} and Database connected`);
+    });
+  }
 } catch (err) {
   console.log(err);
 }
--- weekly_test-5/frontend/src/App.jsx
+++ weekly_test-5/frontend/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @format */
+
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  async function getData() {
+    const response = await axios.get("http://localhost:6565/getData");
+    console.log(response);
+  }
+  getData();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='container min-h-dvh w-full bg-gradient-to-tr from-cyan-200 to-lime-200'>
+      <nav className='w-full py-4 px-2 flex items-center justify-between font-medium border-b-2 border-gray-500 bg-emerald-200'>
+        <h1 className='text-3xl text-rose-500'>PLAN_IT</h1>
+        <form action='' className='flex gap-4 px-8'>
+          <input
+            type='text'
+            placeholder='Task Title....'
+            className=' text-center p-2 outline-orange-300 rounded-lg border-2 border-gray-300'
+          />
+          <input
+            type='text'
+            name=''
+            id=''
+            placeholder='Task detail...'
+            className=' text-center p-2 outline-orange-300 rounded-lg border-2 border-gray-300'
+          />
+          <input
+            type='time'
+            name=''
+            id=''
+            className=' text-center p-2 outline-orange-300 rounded-lg border-2 border-gray-300'
+          />
+          <input
+            type='date'
+            className=' text-center p-2 outline-orange-300 rounded-lg border-2 border-gray-300'
+          />
+          <button>Add Task</button>
+        </form>
+      </nav>
+    </div>
+  );
 }
 
-export default App
+export default App;
